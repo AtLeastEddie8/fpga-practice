c8af3e1815f2af977cdb5c723b6c2fd0d144ca94
AtLeastEddie8 <135277131+AtLeastEddie8@users.noreply.github.com>
2023-06-28 22:10:09 -0400

    Create switchlight.v

---
+++ seven_segment/switchlight.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/28/2023 08:47:27 PM
+// Design Name: 
+// Module Name: switchlight
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module switchlight(
+    input CLK100MHZ,
+    input rst,
+    
+    output reg [6:0] seg,
+    output reg [7:0] AN,
+    output reg [7:0] LED
+    );
+    reg [31:0] counter;
+    reg [6:0] segHOLDER;
+    localparam bit1 = 1'b1;
+    
+    always@(posedge CLK100MHZ) begin
+        if(rst) begin
+            counter <= {32{1'b0}};
+            LED <= {8{1'b0}};
+            AN <= 8'b11111110;
+            seg <= {7{1'b1}};
+        end
+        else begin
+            counter <= counter + 1'b1;
+            segHOLDER <= seg;
+            if(counter[24])begin
+                LED <= LED + 1'b1;
+                seg <= seg - 1'b1;
+                counter <= {32{1'b0}};
+            end
+            else begin
+                LED <= LED;
+                seg <= seg;
+            end
+        end
+    end
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
